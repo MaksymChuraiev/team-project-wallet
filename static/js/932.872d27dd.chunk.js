e5d90e8f0beb21a2e6738d2bfce4cdd5fc33fd7d
MaksymChuraiev <87321814+MaksymChuraiev@users.noreply.github.com>
2022-07-28 14:55:41 +0000

    Deploying to gh-pages from @ MaksymChuraiev/team-project-wallet@d89998b64c23517cf8dd6a929262029d483e4969 🚀

---
+++ static/js/932.872d27dd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkteam_project_wallet=self.webpackChunkteam_project_wallet||[]).push([[932],{932:function(e,t,a){a.r(t);var n=a(184);t.default=function(){return(0,n.jsx)("div",{children:"RegistrationPage"})}}}]);
+//# sourceMappingURL=932.872d27dd.chunk.js.map
