edc7dea21f832c917224c3108509bcf80f1c10a3
MaksymChuraiev <mchuraev1988@gmail.com>
2022-07-29 00:04:37 +0300

    add vars

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
 import { Layout } from './Layout/Layout';
+import ProtectedRoute from './Routes/ProtectedRoute';
+
 const RegisterPage = lazy(() => import('../pages/RegistrationPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage.jsx'));
 const DashboardPage = lazy(() => import('../pages/DashboardPage.jsx'));
@@ -14,7 +16,14 @@ export const App = () => {
           <Route path="registration" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />
         </Route>
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Suspense>
   );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
 // import { store, persistor } from './redux/store';
 import { App } from 'components/App';
-import './index.css';
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
