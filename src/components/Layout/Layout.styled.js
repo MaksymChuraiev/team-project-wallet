907d1eb2ecbd466d9acbed2b262314ec8effe1ee
Yevheniia Ukolova <ukolova_e@ukr.net>
2022-08-02 20:17:13 +0300

    layout

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -4,30 +4,29 @@ import tabletBg from '../../images/tablet-bg-min.png';
 
 export const LayoutStyled = styled.div`
   /* position: fixed;
-  width: 320px;
-  height: 568px;
+  width: 100vw;
+  height: 100vh;
   margin: 0 auto;
   border: 2px solid red; */
 
   @media screen and (min-width: 768px) {
     position: fixed;
-    display:flex;
-    width:768px;
-    height:1024px;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+
     z-index: -1;
-     margin-left: auto;
-        margin-right: auto;
-        background-image: url(${tabletBg});
-        background-repeat: no-repeat;
-        background-size: cover;
-        left: 50%;
+    margin-left: auto;
+    margin-right: auto;
+    background-image: url(${tabletBg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    left: 50%;
     transform: translateX(-50%);
   }
   @media screen and (min-width: 1280px) {
-    width: 1280px;
-    height:720px;
     background-image: url(${bgImage});
-    
+
     /* border: 2px solid red; */
   }
 `;
