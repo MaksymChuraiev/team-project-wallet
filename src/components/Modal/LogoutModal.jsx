bb0c8a014d9c5505db97466021e4ee696f371227
MaksymChuraiev <mchuraev1988@gmail.com>
2022-08-03 19:24:39 +0300

    Update LogoutModal.jsx

--- src/components/Modal/LogoutModal.jsx
+++ src/components/Modal/LogoutModal.jsx
@@ -4,64 +4,64 @@ import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
 import Button from 'components/Button/Button';
 import {
-    ModalStyled,
-    ButtonCloseStyled,
-    ModalTitle,
-} from './ModalAddTransaction.styled';
+  ModalStyled,
+  ButtonCloseStyled,
+  ModalTitle,
+} from './ModalAddTransaction/ModalAddTransaction.styled';
 import { ReactComponent as Close } from '../../icons/close.svg';
-import {logOut} from '../../redux/auth/auth-operation'
+import { logOut } from '../../redux/auth/auth-operation';
 
 Modal.setAppElement('#root');
 
-export const LogoutModal = (modalState) => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [modalIsOpen, setIsOpen] = React.useState(modalState);
+export const LogoutModal = modalState => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [modalIsOpen, setIsOpen] = React.useState(modalState);
 
-    // function openModal() {
-    // setIsOpen(true);
-    // }
-    
-    function closeModal() {
+  // function openModal() {
+  // setIsOpen(true);
+  // }
+
+  function closeModal() {
     setIsOpen(false);
-    }
-    
-    const handleClick = () => {
-        dispatch(logOut());
-        closeModal();
-        navigate('/login');
-    }
+  }
 
-    return (
-        <ModalStyled
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        overlayClassName="modal-overlay"
-        contentLabel="Example Modal"
-        htmlOpenClassName="no-scroll"
-        >
-<ButtonCloseStyled onClick={closeModal}>
-          <Close />
-        </ButtonCloseStyled>
-            <ModalTitle>Are you sure you want to Logout?</ModalTitle>
-            
-            <Button
-            buttonTitle="Logout"
-            onClick={handleClick}
-            type="button"
-            color='#fff'
-            bgColor='#24CCA7'
-            border='none'
-            />
-            
-            <Button
-            buttonTitle="Cancel"
-            onClick={closeModal}
-            type="button"
-            color='#4A56E2'
-            bgColor='#ffffff'
-            borderColor='#4A56E2'
-          />
-        </ModalStyled>
-    )
-}
+  const handleClick = () => {
+    dispatch(logOut());
+    closeModal();
+    navigate('/login');
+  };
+
+  return (
+    <ModalStyled
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      overlayClassName="modal-overlay"
+      contentLabel="Example Modal"
+      htmlOpenClassName="no-scroll"
+    >
+      <ButtonCloseStyled onClick={closeModal}>
+        <Close />
+      </ButtonCloseStyled>
+      <ModalTitle>Are you sure you want to Logout?</ModalTitle>
+
+      <Button
+        buttonTitle="Logout"
+        onClick={handleClick}
+        type="button"
+        color="#fff"
+        bgColor="#24CCA7"
+        border="none"
+      />
+
+      <Button
+        buttonTitle="Cancel"
+        onClick={closeModal}
+        type="button"
+        color="#4A56E2"
+        bgColor="#ffffff"
+        borderColor="#4A56E2"
+      />
+    </ModalStyled>
+  );
+};
