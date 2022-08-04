329c4020eab446e536cbfc527c4e8ce64ee1c857
MaksymChuraiev <mchuraev1988@gmail.com>
2022-08-04 22:23:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,87 +8,85 @@ import HomeTabPage from 'pages/HomeTabPage';
 import { StatisticsPage } from 'pages/StaisticsPage';
 import { Currency } from './Currency/Currency';
 
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 
 const RegisterPage = lazy(() => import('../pages/RegistrationPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage.jsx'));
 const DashboardPage = lazy(() => import('../pages/DashboardPage.jsx'));
 
 export const App = () => {
+  return (
+    <Suspense fallback={<p>Loading..</p>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="registration" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          >
+            <Route
+              path="home-tab"
+              element={
+                <ProtectedRoute>
+                  <HomeTabPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="statistics"
+              element={
+                <ProtectedRoute>
+                  <StatisticsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="banktable"
+              element={
+                <ProtectedRoute>
+                  <Currency />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Route>
+      </Routes>
+    </Suspense>
 
-	return (
-		<Suspense fallback={<p>Loading..</p>}>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route path="registration" element={<RegisterPage />} />
-					<Route path="login" element={<LoginPage />} />
-					<Route
-						path="dashboard"
-						element={
-							<ProtectedRoute>
-								<DashboardPage />
-							</ProtectedRoute>
-						}
-					>
-						<Route
-							path="home-tab"
-							element={
-								<ProtectedRoute>
-									<HomeTabPage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="statistics"
-							element={
-								<ProtectedRoute>
-									<StatisticsPage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="banktable"
-							element={
-								<ProtectedRoute>
-									<Currency />
-								</ProtectedRoute>
-							}
-						/>
-					</Route>
-				</Route>
-			</Routes>
-		</Suspense>
+    // <Suspense fallback={<p>Loading...</p>}>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route path="registration" element={<RegisterPage />} />
+    //       <Route path="login" element={<LoginPage />} />
 
-
-		// <Suspense fallback={<p>Loading...</p>}>
-		//   <Routes>
-		//     <Route path="/" element={<Layout />}>
-		//       <Route path="registration" element={<RegisterPage />} />
-		//       <Route path="login" element={<LoginPage />} />
-
-		//       <Route */}
-		//         path="dashboard"
-		//         element={
-		//           <ProtectedRoute>
-		//             <DashboardPage />
-		//           </ProtectedRoute>
-		//         }
-		//       />
-		//       <Route
-		//         path="home-tab"
-		//         element={
-		//           <ProtectedRoute>
-		//             <HomeTabPage />
-		//           </ProtectedRoute>
-		//         }
-		//       >
-		//         <Route index element={<Home />} />
-		//         <Route path="home" index element={<Home />} />
-		//         <Route path="statistics" element={<Statistics />} />
-		//         <Route path="banktable" element={<Statistics />} />
-		//       </Route>
-		//     </Route>
-		//   </Routes>
-		// </Suspense>
-	);
+    //       <Route */}
+    //         path="dashboard"
+    //         element={
+    //           <ProtectedRoute>
+    //             <DashboardPage />
+    //           </ProtectedRoute>
+    //         }
+    //       />
+    //       <Route
+    //         path="home-tab"
+    //         element={
+    //           <ProtectedRoute>
+    //             <HomeTabPage />
+    //           </ProtectedRoute>
+    //         }
+    //       >
+    //         <Route index element={<Home />} />
+    //         <Route path="home" index element={<Home />} />
+    //         <Route path="statistics" element={<Statistics />} />
+    //         <Route path="banktable" element={<Statistics />} />
+    //       </Route>
+    //     </Route>
+    //   </Routes>
+    // </Suspense>
+  );
 };
