26812ef153583bfee0610aec1905a6431be94164
Yevheniia Ukolova <ukolova_e@ukr.net>
2022-08-05 12:18:00 +0300

    numbers fixed

--- src/components/HomeTab/Table.jsx
+++ src/components/HomeTab/Table.jsx
@@ -1,57 +1,67 @@
 import {
-	TableMain,
-	TableHeader,
-	TableHeaderRow,
-	TableBody,
-	TableRow,
-	TableHeadCell,
-	TableCell,
-	TableCellColor
-} from "./Table.styled"
-import spaceCreator from "services/spaceCreator";
+  TableMain,
+  TableHeader,
+  TableHeaderRow,
+  TableBody,
+  TableRow,
+  TableHeadCell,
+  TableCell,
+  TableCellColor,
+} from './Table.styled';
+import spaceCreator from 'services/spaceCreator';
 
 export const Table = ({ items }) => {
+  console.log(items);
+  const normTime = data => {
+    const year = data.slice(0, 4);
+    const month = data.slice(6, 7).padStart(2, '0');
+    const day = data.slice(8, 10).padStart(2, '0');
 
-	const normTime = (data) => {
-		const year = data.slice(0, 4);
-		const month = data.slice(6, 7).padStart(2, '0');
-		const day = data.slice(8, 10).padStart(2, '0');
+    return `${day}.${month}.${year}`;
+  };
 
-		return `${day}.${month}.${year}`
-	}
-
-	return (
-		<>
-			<TableMain>
-				<TableHeader>
-					<TableHeaderRow>
-						<TableHeadCell>date</TableHeadCell>
-						<TableHeadCell>type</TableHeadCell>
-						<TableHeadCell>category</TableHeadCell>
-						<TableHeadCell>comment</TableHeadCell>
-						<TableHeadCell>sum</TableHeadCell>
-						<TableHeadCell>balance</TableHeadCell>
-					</TableHeaderRow>
-				</TableHeader>
-				<TableBody>
-					{items.map(
-						({ _id, date, transactionType, category, comment, amount, balance }) => (
-							<TableRow key={_id}>
-								<TableCell>{normTime(date)}</TableCell>
-								<TableCell>{transactionType === true ? '+' : '-'}</TableCell>
-								<TableCell>{category}</TableCell>
-								<TableCell>{comment}</TableCell>
-								<TableCell>
-									<TableCellColor
-										type={transactionType === true ? 'income' : 'costs'}>{spaceCreator(amount)}
-									</TableCellColor>
-								</TableCell>
-								<TableCell>{spaceCreator(balance)}</TableCell>
-							</TableRow>
-						),
-					)}
-				</TableBody>
-			</TableMain>
-		</>
-	)
-}
+  return (
+    <>
+      <TableMain>
+        <TableHeader>
+          <TableHeaderRow>
+            <TableHeadCell>date</TableHeadCell>
+            <TableHeadCell>type</TableHeadCell>
+            <TableHeadCell>category</TableHeadCell>
+            <TableHeadCell>comment</TableHeadCell>
+            <TableHeadCell>sum</TableHeadCell>
+            <TableHeadCell>balance</TableHeadCell>
+          </TableHeaderRow>
+        </TableHeader>
+        <TableBody>
+          {items?.map(
+            ({
+              _id,
+              date,
+              transactionType,
+              category,
+              comment,
+              amount,
+              balance,
+            }) => (
+              <TableRow key={_id}>
+                <TableCell>{normTime(date)}</TableCell>
+                <TableCell>{transactionType === true ? '+' : '-'}</TableCell>
+                <TableCell>{category}</TableCell>
+                <TableCell>{comment}</TableCell>
+                <TableCell>
+                  <TableCellColor
+                    type={transactionType === true ? 'income' : 'costs'}
+                  >
+                    {spaceCreator(amount)}
+                  </TableCellColor>
+                </TableCell>
+                <TableCell>{spaceCreator(balance)}</TableCell>
+              </TableRow>
+            )
+          )}
+        </TableBody>
+      </TableMain>
+    </>
+  );
+};
