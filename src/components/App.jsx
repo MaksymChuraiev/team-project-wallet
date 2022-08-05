7b7a3c50acb77ab299a1bd3d8fcdf6d970a3e0d1
AKdimka <dimabrigadir1@gmail.com>
2022-08-05 23:55:19 +0300

    Create BankTablePage whith react-media

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,56 +2,102 @@ import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import { Layout } from './Layout/Layout';
 import ProtectedRoute from './Routes/ProtectedRoute';
+import PublicRoute from './Routes/PublicRoute';
 import HomeTabPage from 'pages/HomeTabPage';
+
+import Loader from '../components/Loader/Loader';
+
+import { ToastContainer } from 'react-toastify';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 // import { Home } from 'components/Routes/Home';
 import { StatisticsPage } from 'pages/StaisticsPage';
-import { Currency } from './Currency/Currency';
+
+import { authOperations } from 'redux/auth';
+import authSelectors from '../redux/auth/auth-selectors'
+
+import BankTablePage from 'pages/BankTablePage';
 
 const RegisterPage = lazy(() => import('../pages/RegistrationPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage.jsx'));
 const DashboardPage = lazy(() => import('../pages/DashboardPage.jsx'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage.jsx'));
 
 export const App = () => {
+	const isRefreshing = useSelector(authSelectors.getIsFetching);
+
+	console.log("Refreshing", isRefreshing);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(authOperations.currentUser());
+	}, [dispatch]);
+
 	return (
-		<Suspense fallback={<p>Loading..</p>}>
+		<Suspense
+			fallback={<Loader />}
+		>
+			<ToastContainer />
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					<Route path="registration" element={<RegisterPage />} />
-					<Route path="login" element={<LoginPage />} />
-					<Route
-						path="dashboard"
-						element={
-							<ProtectedRoute>
-								<DashboardPage />
-							</ProtectedRoute>
-						}
-					>
-						<Route
-							path="home-tab"
-							element={
-								<ProtectedRoute>
-									<HomeTabPage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="statistics"
-							element={
-								<ProtectedRoute>
-									<StatisticsPage />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							path="banktable"
-							element={
-								<ProtectedRoute>
-									<Currency />
-								</ProtectedRoute>
-							}
-						/>
-					</Route>
+
+					{!isRefreshing &&
+						<>
+							<Route index element={
+								<PublicRoute restricted>
+									<LoginPage />
+								</PublicRoute>
+							} />
+							<Route path="registration" element={
+								<PublicRoute restricted>
+									<RegisterPage />
+								</PublicRoute>}
+							/>
+							<Route path="login" element={
+								<PublicRoute restricted>
+									<LoginPage />
+								</PublicRoute>
+							} />
+
+							<Route
+								path="/"
+								element={
+									// <ProtectedRoute>
+									<DashboardPage />
+									// </ProtectedRoute>
+								}
+							>
+								<Route
+									path="home-tab"
+									element={
+										<ProtectedRoute>
+											<HomeTabPage />
+										</ProtectedRoute>
+									}
+								/>
+								<Route
+									path="statistics"
+									element={
+										<ProtectedRoute>
+											<StatisticsPage />
+										</ProtectedRoute>
+									}
+								/>
+								<Route
+									path="banktable"
+									element={
+										<ProtectedRoute>
+											<BankTablePage />
+										</ProtectedRoute>
+									}
+								/>
+							</Route>
+							<Route path="*" element={<NotFoundPage />} />
+						</>
+
+
+					}
 				</Route>
 			</Routes>
 		</Suspense>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,40 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ModalAddTransaction } from '../Modal/ModalAddTransaction';
-import { LayoutStyled, Container } from './Layout.styled';
+
+import { ModalAddTransaction } from '../Modal/ModalAddTransaction/ModalAddTransaction';
+//import { ButtonAddTransactions } from '../ButtonAddTransactions/ButtonAddTransactions';
+
+import { useSelector } from 'react-redux';
+import authSelectors from '../../redux/auth/auth-selectors';
+import Loader from '../../components/Loader/Loader';
+// import { LogoutModal } from 'components/Modal/LogoutModal';
+
+import { LayoutStyled, ContainerForBlur } from './Layout.styled';
 
 export const Layout = () => {
-  return (
-    <>
-      <LayoutStyled>
-        <Container />
-        {/* <h1>Layout</h1> */}
-        <ModalAddTransaction />
-      </LayoutStyled>
+	// const navigate = useNavigate();
+	const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-      <Suspense fallback={<p>Loading...</p>}>
-        <Outlet />
-      </Suspense>
-    </>
-  );
+	// useEffect(() => {
+	//   return navigate('/login');
+	// }, []);
+	return (
+		<>
+			<LayoutStyled>
+				{!isLoggedIn ? (
+					<ContainerForBlur left="45%" />
+				) : (<>
+					<ContainerForBlur left="0px" />
+					{/* <ButtonAddTransactions /> */}
+				</>
+
+				)}
+
+				<ModalAddTransaction />
+			</LayoutStyled>
+			<Suspense fallback={<Loader />}>
+				<Outlet />
+			</Suspense>
+		</>
+	);
 };
